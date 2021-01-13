42144c667b4e34adce6bb6e5f153bf5c548a0a65
hub851 <hub851@gmail.com>
2021-01-14 09:16:41 +0930

    wouldn't run. error with fill colour. added speed changed range

--- flower.py
+++ flower.py
@@ -1,8 +1,12 @@
 import turtle
+from turtle import*
+
 
 t = turtle.Turtle()
-for i in range(100):
-    t.begin_fill("blue")
+for i in range(8): #limits repeat of circles to 8
+    t.speed(50) #modify speed of turtle
+    t.fillcolor('blue')
+    t.begin_fill()
     t.circle(50)
     t.left(45)
     t.end_fill()

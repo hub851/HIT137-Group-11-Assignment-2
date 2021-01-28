94e50e7b9820382e6628731c8d98dce6c838c20e
hub851 <hub851@gmail.com>
2021-01-28 11:10:55 +0930

    trying to update code might need to revert to csv write

--- Part 2 testing inventory xlsx.py
+++ Part 2 testing inventory xlsx.py
@@ -2,6 +2,8 @@ from tkinter import *
 import xlsxwriter
 import datetime
 
+
+
 def enter_button():
     now = datetime.datetime.now()
     SKU = e1.get()
@@ -9,17 +11,28 @@ def enter_button():
     BIN = e3.get()
     Location = e4.get()
     QTY = e5.get()
-    with open('File.xlsx', 'a') as f:
-        w = csv.writer(f,dialect='excel-tab')
-        w.writerow([now.strftime("%Y-%m-%d %H:%M"), SKU, Description, BIN, Location, QTY])
+    #with open('File.xlsx', 'a') as f: #this needs to be fixed to work with excel
+
+workbook = xlsxwriter.Workbook('File.xlsx')
+worksheet = workbook.add_worksheet()
+row=0
+column=0
+for type, input in (enter_button): #needs to execute from enter button
+    worksheet.write(row, col, type)
+    worksheet.write(row, col + 1, input)
+    row += 1
+
+        #w = xlsx.writer(f,dialect='excel-tab')
+        #w.writerow([now.strftime("%Y-%m-%d %H:%M"), SKU, Description, BIN, Location, QTY])
 
 window=Tk()
 window.title("Inventory Input")
-
+window.geometry('300x170')
 
 Label(window, text='SKU').grid(row=0)
 e1 = Entry(window)
-e1.grid(column=1, row=0)
+e1.grid(column=1, row=0, columnspan=4) #want to expand the column span columnspan func not working
+
 Label(window, text='Description').grid(row=1)
 e2 = Entry(window)
 e2.grid(column=1, row=1)
@@ -33,7 +46,6 @@ Label(window, text='QTY').grid(row=4)
 e5 = Entry(window)
 e5.grid(column=1, row=4)
 myButton=Button(window,text='Enter',command=enter_button)
-e1.grid(row=0,column=1)
 myButton.grid(row=5,column=0)
 
 mainloop()

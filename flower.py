d846e5a747a877105a9d7586f88c1ac1305378d9
agreen-cdu <77263863+agreen-cdu@users.noreply.github.com>
2021-01-31 17:49:32 +1100

    Add files via upload

--- flower.py
+++ flower.py
@@ -1,12 +1,8 @@
 import turtle
-from turtle import*
-
 
 t = turtle.Turtle()
-for i in range(8): #limits repeat of circles to 8
-    t.speed(50) #modify speed of turtle
-    t.fillcolor('blue')
-    t.begin_fill()
+for i in range(100):
+    t.begin_fill("blue")
     t.circle(50)
     t.left(45)
     t.end_fill()
